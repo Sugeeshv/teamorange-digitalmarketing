ef6555e1e2d90e5a31b146926396725b94ed7c69
sugeesh.vk@eljoservices.com <sugeeshvk541@gmail.com>
2024-08-07 12:15:15 +0530

    .

--- database/migrations/2024_08_03_114805_create_services_table.php
+++ database/migrations/2024_08_03_114805_create_services_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('description');
             $table->string('thumbnail');
             $table->string('img');
+            $table->string('url')->nullable();
             $table->timestamps();
         });
     }
