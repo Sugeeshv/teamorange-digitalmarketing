c13683bce7e80cd47620255c7d0794478ae6b490
sugeesh.vk@eljoservices.com <sugeeshvk541@gmail.com>
2024-08-08 16:21:49 +0530

    .

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -110,5 +110,14 @@
     <script>
   AOS.init();
 </script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+    
+    <!-- Custom JavaScript to open modal on page load -->
+    <script>
+      document.addEventListener('DOMContentLoaded', function () {
+        var exampleModal = new bootstrap.Modal(document.getElementById('exampleModal'));
+        exampleModal.show();
+      });
+    </script>
   </body>
 </html>
--- resources/views/web/services.blade.php
+++ resources/views/web/services.blade.php
@@ -49,4 +49,22 @@
         </div>
       </section>
    
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-md">
+        <div class="modal-content">
+          <div class="modal-header">
+            <!-- <h5 class="modal-title" id="exampleModalLabel">Modal title</h5> -->
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+           <img  style="height:80vh" src="{{asset('web/images/modal.jpeg')}}" alt="">
+          </div>
+          <!-- <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div> -->
+        </div>
+      </div>
+    </div>
       @endsection
